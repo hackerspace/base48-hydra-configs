53a66eb84fb7eae20c35b1142874e47596917de9
Richard Marko <srk@48.io>
2018-02-17 14:35:57 +0100

    initial

---
+++ vpsadminos/default.nix
@@ -0,0 +1,42 @@
+{ }:
+
+let
+  pkgs = import <nixpkgs>{};
+in with (import ../lib.nix { inherit pkgs; });
+with pkgs.lib;
+let
+  defaults = globalDefaults // {
+    nixexprinput = "vpsadminos";
+    nixexprpath = "os/release.nix";
+  };
+  primary_jobsets = {
+    vpsadminos-unstable = defaults // {
+      description = "vpsadminos-unstable";
+      inputs = {
+        vpsadminos = mkFetchGithub "https://github.com/vpsfreecz/vpsadminos master";
+        nixpkgs = mkFetchGithub "https://github.com/nixos/nixpkgs-channels.git nixos-unstable-small";
+        supportedSystems2 = { type = "nix"; value = ''[ "x86_64-linux" ]''; emailresponsible = false; };
+      };
+    };
+    vpsadminos-master = defaults // {
+      description = "vpsadminos-master";
+      inputs = {
+        vpsadminos = mkFetchGithub "https://github.com/vpsfreecz/vpsadminos master";
+        nixpkgs = mkFetchGithub "https://github.com/nixos/nixpkgs.git master";
+        supportedSystems2 = { type = "nix"; value = ''[ "x86_64-linux" ]''; emailresponsible = false; };
+      };
+    };
+    vpsadminos-sorki = defaults // {
+      description = "vpsadminos-sorki";
+      inputs = {
+        vpsadminos = mkFetchGithub "https://github.com/vpsfreecz/vpsadminos master";
+        nixpkgs = mkFetchGithub "https://github.com/sorki/nixpkgs.git vpsadminos";
+        supportedSystems2 = { type = "nix"; value = ''[ "x86_64-linux" ]''; emailresponsible = false; };
+      };
+    };
+  };
+
+  jobsetsAttrs = primary_jobsets;
+in {
+  jobsets = pkgs.writeText "spec.json" (builtins.toJSON jobsetsAttrs);
+}
